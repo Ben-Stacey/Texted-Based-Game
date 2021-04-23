222930f887433db2b922b0c5e31c91528a424d88
Ben-Stacey <benjaminstacey87gmail.com>
2021-04-23 15:52:55 +1200

    staged

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[]args){
+        HelloWorld h = new HelloWorld();
+        h.hello();
+    }
+
+    public void hello(){
+        print("Hello World");
+    }
+
+    public void print(String p){
+        System.out.println(p);
+    }
+}
